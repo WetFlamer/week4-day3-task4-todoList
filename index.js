66e838f406cfd571b3a6b9abe735d1106883aec0
WetFlamer <xamidchabaev04@gmail.com>
2022-11-18 17:08:29 +0300

    Task Completed

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // ADD ELEMENT
 const add_button = document.getElementById("add-button");
 add_button.addEventListener("click", () => {
-  // INPUT
-  const input = document.getElementsByTagName("input")[0];
+  const input = document.getElementById("input");
   //DIV0
   const div0 = document.getElementById("list");
   const div = document.createElement("div");
@@ -10,15 +9,30 @@ add_button.addEventListener("click", () => {
   const p = document.createElement("p");
   p.textContent = input.value;
   p.className = "item-text";
+  
+  // КНОПКА DELETE
   const button = document.createElement("button");
-  button.className = "delete-item"
-  button.addEventListener('click', (e) => {
-    e.target.parentElement.remove()
-  })
-  //BUTTON
+  button.className = "delete-item";
+  button.addEventListener("click", (e) => {
+    e.target.parentElement.remove();
+  });
+  // CHECKBOX
+  const check = document.createElement("input")
+  check.type = 'checkbox'
+  check.addEventListener("click", (e) => {
+
+    if (check.checked) {
+      e.target.parentElement.className = "checkB";
+    } else {
+      e.target.parentElement.className = "item-1";
+    }
+  });
+
+
 
   // ВЫВОД
-  div.append(p, button);
+  div.append(p, button, check);
   div0.prepend(div);
   input.value = "";
 });
+
